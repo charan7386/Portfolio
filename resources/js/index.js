26359a52e1b217c559d5aac2fc60429cfd98517d
charan7386 <gummadavallicharansai12@gmail.com>
2018-08-28 20:18:26 +0530

    Added Skills Section

---
+++ resources/js/index.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('.chart').easyPieChart({
+    scaleColor: "#ecf0f1",
+    lineWidth: 20,
+    lineCap: 'butt',
+    barColor: '#fda085',
+    trackColor:	"#ecf0f1",
+    size: 190,
+    animate: 500
+  });
+});
